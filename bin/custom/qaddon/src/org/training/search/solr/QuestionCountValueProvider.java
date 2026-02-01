861e42b0e720923dd70debc08cfd49c4a91ecf68
toss1ing <kirill.borisenko05@mail.ru>
2026-02-01 23:55:48 +0300

    Added Solr facets and sorting for question count

---
+++ bin/custom/qaddon/src/org/training/search/solr/QuestionCountValueProvider.java
@@ -0,0 +1,22 @@
+package org.training.search.solr;
+
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.config.exceptions.FieldValueProviderException;
+import de.hybris.platform.solrfacetsearch.indexer.IndexerBatchContext;
+import de.hybris.platform.solrfacetsearch.indexer.spi.InputDocument;
+import de.hybris.platform.solrfacetsearch.provider.impl.AbstractValueResolver;
+
+public class QuestionCountValueProvider extends AbstractValueResolver<ProductModel, Object, Object> {
+
+    @Override
+    protected void addFieldValues(InputDocument inputDocument, IndexerBatchContext indexerBatchContext,
+                                  IndexedProperty indexedProperty, ProductModel productModel,
+                                  ValueResolverContext<Object, Object> valueResolverContext) throws FieldValueProviderException {
+        int count = productModel.getQuestions() == null ? 0 : productModel.getQuestions().size();
+        inputDocument.addField(indexedProperty, count, valueResolverContext.getFieldQualifier());
+    }
+
+}
+
+
